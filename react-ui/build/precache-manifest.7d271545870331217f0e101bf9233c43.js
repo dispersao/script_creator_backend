561e7e7917406edb0272a1ac9b72e5b2f9856f0f
mairasala <mairasala@gmail.com>
2019-04-28 12:33:19 +0200

    include frontend in repo

---
+++ react-ui/build/precache-manifest.7d271545870331217f0e101bf9233c43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e2d95414cfbad5d8fae6",
+    "url": "/static/js/main.e2d95414.chunk.js"
+  },
+  {
+    "revision": "be43b29f30196176b7fd",
+    "url": "/static/js/2.be43b29f.chunk.js"
+  },
+  {
+    "revision": "e2d95414cfbad5d8fae6",
+    "url": "/static/css/main.4505589f.chunk.css"
+  },
+  {
+    "revision": "6b5877711602974408b39833597cc070",
+    "url": "/index.html"
+  }
+];
