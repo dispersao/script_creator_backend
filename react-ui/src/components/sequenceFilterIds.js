ab984e2fe29d4a871f6e32c5f2cc5b156906a842
mairasala <mairasala@gmail.com>
2019-07-19 13:01:25 +0200

    order filters

--- react-ui/src/components/sequenceFilterIds.js
+++ react-ui/src/components/sequenceFilterIds.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Select from 'react-select'
+import sortBy from 'lodash/orderBy'
 
-const SequenceFilterIds = ({filter, items, onChangeIds}) => {
+
+const SequenceFilterIds = ({filter, items, onChangeIds, sortByField}) => {
   return (
       <Select
        isMulti = 'true'
        value={idsToOption(items, filter.ids)}
        onChange={els => onChangeIds(els.map(el => el.value))}
-       options={itemsToOptions(items)}
+       options={itemsToOptions(items, sortByField)}
      />
   )
 }
@@ -16,14 +18,18 @@ export default SequenceFilterIds
 const idsToOption = (items, ids) => {
   if(ids && ids.length){
     return ids.map(id => {
-       return itemToOption(items[id.toString()])
+      return itemToOption(items[id.toString()])
     })
   }
 }
 
-const itemsToOptions = (items) => {
+const itemsToOptions = (items, sortByField) => {
   if(items && Object.keys(items).length){
-    return Object.keys(items).map(key => itemToOption(items[key]))
+    return sortBy(
+      Object.keys(items).map(key => (itemToOption(items[key]))),
+      el => sortByField ? (Number.isNaN(Number(el[sortByField])) ? el[sortByField] : Number(el[sortByField])) : el
+    )
+
   }
 }
 
--- react-ui/src/components/sequenceFilters.js
+++ react-ui/src/components/sequenceFilters.js
@@ -5,11 +5,11 @@ import SequenceCategoryFilter from './sequenceCategoryFilter'
 const SequenceFilters = (props)=>{
   return (
     <section className="FiltersContainer">
-      <SequenceFilter name='characters' key={1}></SequenceFilter>
-      <SequenceFilter name='types' key={2}></SequenceFilter>
-      <SequenceFilter name='locations' key={3}></SequenceFilter>
-      <SequenceCategoryFilter name='arc' key={4}></SequenceCategoryFilter>
-      <SequenceCategoryFilter name='pos' key={5}></SequenceCategoryFilter>
+      <SequenceFilter name='characters' key={1} sortByField='value'></SequenceFilter>
+      <SequenceFilter name='types' key={2} sortByField='value'></SequenceFilter>
+      <SequenceFilter name='locations' key={3} sortByField='label'></SequenceFilter>
+      <SequenceCategoryFilter name='arc' key={4} sortByField='label'></SequenceCategoryFilter>
+      <SequenceCategoryFilter name='pos' key={5} sortByField='label'></SequenceCategoryFilter>
     </section>
   )
 }
